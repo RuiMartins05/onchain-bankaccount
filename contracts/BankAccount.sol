f456ebd9fc299afdea85424e2c6b8e73f3b59696
fc56283 <fc56283@alunos.fc.ul.pt>
2023-07-24 21:15:05 +0100

    displayed balance and owner

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -3,34 +3,34 @@ pragma solidity ^0.8.20;
 
 contract BankAccount {
 
-    address payable private owner;
+    address payable public owner;
 
     constructor() payable {
         owner = payable(msg.sender);
     }
 
-    modifier onlyOnwer {
-        require(msg.sender == owner, "NOT THE ONWER");
-        _;
-    }
+    //modifier onlyOnwer {
+    //    require(msg.sender == owner, "NOT THE ONWER");
+    //    _;
+    //}
 
-    function deposit() external payable onlyOnwer {}
+    function deposit() external payable {}
 
-    function checkBalance() external view onlyOnwer returns(uint256) {
+    function checkBalance() external view returns(uint256) {
         return address(this).balance;
     }
 
-    function withdraw(uint256 amountInEth) external payable onlyOnwer {
+    function withdraw(uint256 amountInEth) external payable {
         uint256 amountInWei = amountInEth * 1 ether;
         require(address(this).balance >= amountInWei, "NOT ENOUGH BALANCE");
         owner.transfer(amountInWei);
     }
 
-    function withdrawAll() external payable onlyOnwer {
+    function withdrawAll() external payable {
         owner.transfer(address(this).balance);
     }
 
-    function sendTo(address payable _to, uint256 amountInEth) external payable onlyOnwer {
+    function sendTo(address payable _to, uint256 amountInEth) external payable {
         uint256 amountInWei = amountInEth * 1 ether;
         require(address(this).balance >= amountInWei, "Insufficient balance");
         
